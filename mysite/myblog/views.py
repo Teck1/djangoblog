955d42ed28c6e8ef2163a53cabacea6ca66aadd9
Teck1 <Teck1@users.noreply.github.com>
2018-08-08 14:50:45 -0700

    Add API functionality for Users, Groups, Posts and Categories

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from myblog.models import Post
+from myblog.models import Post, Category
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
 def stub_view(request, *args, **kwargs):
